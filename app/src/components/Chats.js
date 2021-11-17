30d7b2efdad1dc1f0ce46435716bfd7c7572c52d
Suraj Singh <jatarogiovani@gmail.com>
2021-11-17 18:43:08 +0530

    chatengine setup

--- app/src/components/Chats.js
+++ app/src/components/Chats.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import {useHistory} from 'react-router-dom';
+import {ChatEngine} from 'react-chat-engine';
+import {auth} from '../firebase';
 function Chats() {
+
+    const history = useHistory();
+    const handleLogout = async () => {
+        await auth.signOut();
+        history.push('/')
+    }
     return (
-        <div>
-            <h1>Chat App</h1>
+        <div className='chats-page'>
+            <div className ='logo-tab'>
+                ChatApp
+            </div>
+            <div onClick ={handleLogout} className='logout-tab'>
+                Logout
+            </div>
+            <ChatEngine 
+                height = 'calc(100vh-66px)'
+                projectId='77e687cf-f7b4-418c-a8e6-95df4c4e9af6'
+                userName = '.'
+                userSecret = '.'
+            />    
         </div>
     )
 }
